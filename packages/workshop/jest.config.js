f490d7e6548ae230be633d10bf3d0efde54eb80c
Ilya Strekalov <ilya.strekalov@fulldive.com>
2023-06-08 15:14:16 +0300

    speed-up tests running time

--- packages/workshop/jest.config.js
+++ packages/workshop/jest.config.js
@@ -2,7 +2,11 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   transform: {
-    '^.+\\.ts?$': 'ts-jest',
+    '^.+\\.ts?$': [
+      'ts-jest', {
+        isolatedModules: true,
+      },
+    ],
   },
   transformIgnorePatterns: ['<rootDir>/node_modules/'],
 };
